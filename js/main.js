0892547b6bb5c885421b19f72bc13ccf23e2fbcd
k1ttyyz <luckyxd136@gmail.com>
2023-11-20 17:23:16 -0400

    aplicação do resto do codigo

--- js/main.js
+++ js/main.js
@@ -17,9 +17,34 @@ let posicaoInicial;
 const capturarLocalizacao = document.getElementById('localizacao');
 const latitude = document.getElementById('latitude');
 const longitude = document.getElementById('longitude');
+const map = document.getElementById('mapa');
 
 const sucesso = (posicao) => {
   posicaoInicial = posicao;
   latitude.innerHTML = posicaoInicial.coords.latitude;
-  
+  longitude.innerHTML = posicaoInicial.coords.longitude;
+  map.src = "http://maps.google.com/maps?q=" + posicaoInicial.coords.latitude + "," + posicaoInicial.coords.longitude + "&z=16&output=embed";
 }
+
+const erro = (error) => {
+  let errorMessage;
+  switch(error.code){
+    case 0:
+      errorMessage = "Erro desconhecido"
+    break;
+    case 1:
+      errorMessage = "Permissão negada!"
+    break;
+    case 2:
+      errorMessage = "Permissão negada!"
+    break;
+    case 3:
+      errorMessage = "Permissão negada!"
+    break;
+    }
+    console.log('Ocorreu um erro:'+ errorMessage);
+};
+
+capturarLocalizacao.addEventListener('click', () => {
+  navigator.geolocation.getCurrentPosition(sucesso, erro);
+});
